618ac010145db976448f4e5b3eec84e0ca8ea664
GauravDS <gaurav.sharma@punchh.com>
2018-09-10 18:51:31 +0530

    removed unused codes.

--- ios/RCTConvert+RNPStatus.m
+++ ios/RCTConvert+RNPStatus.m
@@ -12,17 +12,8 @@
 
 RCT_ENUM_CONVERTER(RNPType, (@{ @"location" : @(RNPTypeLocation),
                                 @"camera" : @(RNPTypeCamera),
-                                @"microphone" : @(RNPTypeMicrophone),
                                 @"photo" : @(RNPTypePhoto),
-                                @"contacts" : @(RNPTypeContacts),
-                                @"event" : @(RNPTypeEvent),
-                                @"reminder" : @(RNPTypeReminder),
-                                @"bluetooth" : @(RNPTypeBluetooth),
-                                @"notification" : @(RNPTypeNotification),
-                                @"backgroundRefresh": @(RNPTypeBackgroundRefresh),
-                                @"speechRecognition": @(RNPTypeSpeechRecognition),
-                                @"mediaLibrary": @(RNPTypeMediaLibrary),
-                                @"motion": @(RNPTypeMotion)
+                                @"notification" : @(RNPTypeNotification)
                                 }),
                                 RNPTypeUnknown, integerValue)
 
